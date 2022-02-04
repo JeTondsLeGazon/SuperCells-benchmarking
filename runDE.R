da321cf0c3d2b9b32a373761c439d2d6f1f19c2d
michelhugo <hugo.michel@epfl.ch>
2022-02-04 10:36:50 +0100

    Cleaning Metacells DE

--- runDE.R
+++ runDE.R
@@ -90,7 +90,7 @@ bulk_data <- readRDS(file = file.path(data_folder, "bulkFilteredNormalized.rds")
 # DE bulk
 # ---------------------------------------------------------
 if(computeBulk){
-    message('Computing Bulk DE genes')
+    message('Computing Bulk DE')
     for(algo in algos){
         DE <- compute_DE_bulk(data = bulk_data, 
                         labels = bulk_data$label,
@@ -101,7 +101,7 @@ if(computeBulk){
                     base.path = results_folder,
                     kind = 'bulk')
     }
-    message('Done computing Bulk DE genes')
+    message('Done computing Bulk DE')
 }
 
 
@@ -109,7 +109,7 @@ if(computeBulk){
 # DE pseudobulk
 # ---------------------------------------------------------
 if(computePseudo){
-    message('Computing Pseudo-bulk DE genes')
+    message('Computing Pseudo-bulk DE')
     for(algo in algos){
         DE <- compute_DE_bulk(data = pseudobulk_data, 
                               labels = pseudobulk_data$label,
@@ -120,7 +120,7 @@ if(computePseudo){
                     base.path = results_folder,
                     kind = 'pseudo')
     }
-    message('Done computing Pseudo-bulk DE genes')
+    message('Done computing Pseudo-bulk DE')
 }
 
 
@@ -130,7 +130,7 @@ if(computePseudo){
 # TODO: change this to have each algo ran separately to save them and not lose
 # everything if crash happens
 if(computeSuper){
-    message('Computing SuperCell DE genes')
+    message('Computing SuperCell DE')
     memory.limit(size=56000)
     data <- single_data
     DEs <- list()
@@ -152,7 +152,7 @@ if(computeSuper){
                     base.path = results_folder,
                     kind = 'super')
     }
-    message('Done computing SuperCell DE genes')
+    message('Done computing SuperCell DE')
 }
 
 
@@ -161,7 +161,7 @@ if(computeSuper){
 # ---------------------------------------------------------
 # TODO: include seurat computation as algorithm
 if(computeSingle){
-    message('Computing Single cells DE genes with Seurat')
+    message('Computing Single cells DE')
     for(algo in algos){
         DE <- compute_DE_single(single_data, algo)
         saveMarkers(markers = DE, 
@@ -170,7 +170,7 @@ if(computeSingle){
                     base.path = results_folder,
                     kind = 'single')
     }
-    message('Done computing Single cells DE genes with Seurat')
+    message('Done computing Single cells DE')
 }
 
 
@@ -179,14 +179,14 @@ if(computeSingle){
 # ---------------------------------------------------------
 # TODO: like supercells
 if(computeMeta){
-    message('Computing MetaCells DE genes with t-test')
+    message('Computing MetaCells DE')
     mc.type <- paste('mc', split.by, sep = '_')
     mc <- readRDS(file.path(data_folder, mc.type, 'mc_default.rds'))
     mc_gammas <- names(mc)
     DEs <- list()
     for(mc_gamma in mc_gammas){
         for(algo in algos){
-            DE <- compute_DE_meta()
+            DE <- compute_DE_meta(mc[[mc_gamma]], algo)
             DEs[[algo]][[mc_gamma]] <- DE
         }
     }
@@ -197,114 +197,36 @@ if(computeMeta){
                     base.path = results_folder,
                     kind = 'meta')
     }
-    message('Done computing MetaCells DE genes with t-test')
+    message('Done computing MetaCells DE')
 }
 
 
 # ---------------------------------------------------------
 # Metacell via SuperCell t-test
 # ---------------------------------------------------------
-if(computeMetaSuper){
-    # Metacell own GE matrix from pipeline, manual t-test
-    message('Computing MetaCells SC-like genes with t-test')
-    mc.type <- paste('mc', split.by, sep = '_')
-    mc <- readRDS(file.path(data_folder, mc.type, 'mc_SC_like.rds'))
-    mc_gammas <- names(mc)
-    DEs <- list()
-    data <- sc_filtered_data
-    for(mc_gamma in mc_gammas){
-        cells.use <- colnames(data)[colnames(data) %in% names(mc[[mc_gamma]]$membership)]
-        ge <- supercell_GE(ge = GetAssayData(data)[, cells.use],
-                     groups =  mc[[mc_gamma]]$membership[cells.use])
-        colnames(ge) <- seq_len(ncol(ge))
-        meta <- data.frame(label = mc[[mc_gamma]]$sample, row.names = colnames(ge))
-        sc <- CreateSeuratObject(counts = ge, meta.data = meta)
-        Idents(sc) <- 'label'
-        # Run DE
-        metacell_markers_manual <- find_markers(sc, stat.test)  # normalized = T
-        DEs[[as.character(mc_gamma)]] <- metacell_markers_manual
-    }
-    saveMarkers(markers = DEs, 
-                algo = 't-test',
-                split.by = split.by,
-                base.path = results_folder,
-                kind = 'metasc')
-    message('Done computing MetaCells SC-like genes with t-test')
-}
-
-
-# ---------------------------------------------------------
-# Metacell via SuperCell DESeq2
-# ---------------------------------------------------------
-if(computeMetaSuperDes){
-    # Metacell own GE matrix from pipeline, DESeq2
-    message('Computing MetaCells SC-like genes with DESeq2')
+# TODO: like supercells
+if(computeMetaSC){
+    message('Computing MetaCells SC-like DE')
     mc.type <- paste('mc', split.by, sep = '_')
     mc <- readRDS(file.path(data_folder, mc.type, 'mc_SC_like.rds'))
     mc_gammas <- names(mc)
     DEs <- list()
     for(mc_gamma in mc_gammas){
-        cells.use <- colnames(data)[colnames(data) %in% names(mc[[mc_gamma]]$membership)]
-        ge <- supercell_GE(ge = GetAssayData(data)[, cells.use],
-                           groups =  mc[[mc_gamma]]$membership[cells.use])
-        labels <- data.frame(label = mc[[mc_gamma]]$sample, 
-                             row.names = names(mc[[mc_gamma]]$sample))
-        # Run DE
-        ge <- floor(sweep(ge, 2, mc[[mc_gamma]]$size, '*'))
-        dds <- DESeqDataSetFromMatrix(ge,
-                                      colData = labels, 
-                                      design = ~ label)
-        
-        dds_wald <- DESeq(dds, test = 'Wald', minReplicatesForReplace = Inf)
-        results_wald <- results(dds_wald)
-        
-        DEs[[as.character(mc_gamma)]] <- arrangeDE(results_wald, 
-                                                   oldNameLog = 'log2FoldChange',
-                                                   oldNameP = 'padj')
+        for(algo in algos){
+            DE <- compute_DE_metasc(data = mc[[mc_gamma]],
+                                    single_data = single_data,
+                                    algo = algo)
+            DEs[[algo]][[mc_gamma]] <- DE
+        }
     }
-    saveMarkers(markers = DEs, 
-                algo = 'DESeq2',
-                split.by = split.by,
-                base.path = results_folder,
-                kind = 'metasc')
-    message('Done computing MetaCells SC-like genes with DESeq2')
-}
-
-
-# ---------------------------------------------------------
-# Metacell via SuperCell EdgeR
-# ---------------------------------------------------------
-if(computeMetaSuperEdge){
-    # Metacell own GE matrix from pipeline, EdgeR
-    message('Computing MetaCells SC-like genes with EdgeR')
-    mc.type <- paste('mc', split.by, sep = '_')
-    mc <- readRDS(file.path(data_folder, mc.type, 'mc_SC_like.rds'))
-    mc_gammas <- names(mc)
-    DEs <- list()
-    for(mc_gamma in mc_gammas){
-        cells.use <- colnames(data)[colnames(data) %in% names(mc[[mc_gamma]]$membership)]
-        ge <- supercell_GE(ge = GetAssayData(data)[, cells.use],
-                           groups =  mc[[mc_gamma]]$membership[cells.use])
-        labels <- mc[[mc_gamma]]$sample
-        
-        # Run DE
-        ge <- floor(sweep(ge, 2, mc[[mc_gamma]]$size, '*'))
-        edge <- DGEList(counts = ge, group = labels)
-        edge <- calcNormFactors(edge)
-        model <- model.matrix(~labels)
-        edge <- estimateDisp(edge, model)
-        
-        # Quasi likelihood test
-        fit <- glmQLFit(edge, model)
-        DEs[[as.character(mc_gamma)]] <- arrangeDE(glmQLFTest(fit, coef= 2)$table,
-                                                   oldNameP = 'PValue')
+    for(algo in algos){
+        saveMarkers(markers = DEs[[algo]], 
+                    algo = algo,
+                    split.by = split.by,
+                    base.path = results_folder,
+                    kind = 'metasc')
     }
-    saveMarkers(markers = DEs, 
-                algo = 'EdgeR',
-                split.by = split.by,
-                base.path = results_folder,
-                kind = 'metasc')
-    message('Done computing MetaCells SC-like genes with EdgeR')
+    message('Done computing MetaCells SC-like DE')
 }
 
 
--- src/analysis.R
+++ src/analysis.R
@@ -39,6 +39,31 @@ compute_DE_meta <- function(data, algo){
     return(DE)
 }
 
+compute_DE_metasc <- function(data, single_data, algo){
+    
+    # Metacell drops some cells along the way
+    cells.use <- colnames(data)[colnames(data) %in% names(mc[[mc_gamma]]$membership)]
+    
+    # Input groups of Metacell into SuperCell GE computation
+    ge <- supercell_GE(ge = single_data@assays$RNA@data[, cells.use],
+                       groups =  data[[mc_gamma]]$membership[cells.use])
+    
+    # Use normalized counts (exm1(ge)) as input to compute normalized average counts
+    counts <- supercell_GE(ge = expm1(single_data@assays$RNA@data[, cells.use]),
+                           groups =  data[[mc_gamma]]$membership[cells.use])
+    
+    if(algo == 'DESeq2'){
+        DE <- computeDESeq2(counts, labels)
+    }else if(algo == 'EdgeR'){
+        DE <- computeEdgeR(counts, labels)
+    }else if(algo == 't-test'){
+        DE <- find_markers(ge, labels)
+    }else{
+        stop('Cannot compute DE of expression, algorithm passed unknown')
+    }
+    return(DE)
+}
+
 
 
 # Computes the match between the topn genes between two sets, normalized [0, 1]

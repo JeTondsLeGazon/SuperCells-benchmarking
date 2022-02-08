ddfc69e043e87f4faae583a1bf17c9ecaae09e98
michelhugo <hugo.michel@epfl.ch>
2022-02-08 16:00:08 +0100

    Add test to compare supercell, random and subsampling

---
+++ tests/test.R
@@ -0,0 +1,57 @@
+# 8 February 2022
+
+# Tests for different functions of SuperCells-benchmarking and implemented 
+# modifications of SupercellsBM
+library(Seurat)
+library(SuperCellBM)
+library(SuperCell)
+library(testthat)
+
+source('src/utility.R')
+source('src/supercells.R')
+source('src/analysis.R')
+source('src/compute_de.R')
+
+single_data <- readRDS('data/hagai_mouse_lps_data/singleCellClusteredNormalized.rds')
+set.seed(0)
+
+
+SC.list <- compute_supercells(
+    sc = single_data,
+    ToComputeSC = T,
+    data.folder = 'data',
+    filename = 'tmp',
+    gamma.seq = 1,
+    n.var.genes = 1000,
+    k.knn = 5,
+    n.pc = 10,
+    approx.N = 1000,
+    fast.pca = TRUE,
+    genes.use = NULL, 
+    genes.exclude = NULL,
+    seed.seq = c(0),
+    split.by = 'label'
+)
+super.random <- SC.list$Random$`1`$`0`
+super.exact <- SC.list$Exact$`1`$`0`
+
+test_that("Unique membership", {
+    expect_equal(length(super.random$membership), length(unique(super.random$membership)))
+    expect_equal(length(super.exact$membership), length(unique(super.exact$membership)))
+})
+
+ge.exact <- supercell_GE(single_data@assays$RNA@data, super.exact$membership)
+colnames(ge.exact) <- super.exact$SC.cell.annotation.
+ge.random <- supercell_GE(single_data@assays$RNA@data, super.random$membership)
+colnames(ge.random) <- super.random$SC.cell.annotation.
+
+test_that("Same rowsums", {
+    expect_equal(rowSums(ge.exact), rowSums(ge.random))
+})
+
+test_that("Same Mean per group", {
+    expect_equal(rowMeans(ge.exact[, colnames(ge.exact) == 'ctrl']), 
+                 rowMeans(ge.random[, colnames(ge.random) == 'ctrl']))
+    expect_equal(rowMeans(ge.exact[, colnames(ge.exact) == 'treat']), 
+                 rowMeans(ge.random[, colnames(ge.random) == 'treat']))
+})
